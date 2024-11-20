927b63ce76da6781938da6e8ddca463d3e296d76
RizalPradanaaa <rizalpradanaaa@gmail.com>
2024-11-20 15:59:43 +0700

    Install Shadcn & navbar

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,49 @@
+import { NavigationMenu, NavigationMenuLink, navigationMenuTriggerStyle } from './ui/navigation-menu'
+import { Sheet, SheetContent, SheetHeader, SheetTitle, SheetTrigger } from './ui/sheet'
+
+export default function Navbar() {
+  return (
+    <div className="flex items-center justify-between w-full px-4 font-poppins font-bold">
+        <NavigationMenu>
+                <img src="./public/logo.png" alt="Logo" width={60}/>
+        </NavigationMenu>
+        <NavigationMenu className='hidden md:flex'>
+            <div className="flex gap-4">
+                <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                    Home
+                </NavigationMenuLink>
+                <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                About
+                </NavigationMenuLink>
+                <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                Skills
+                </NavigationMenuLink>
+                <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                Projects
+                </NavigationMenuLink>
+                <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                Sertificate
+                </NavigationMenuLink>
+                <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                Contact
+                </NavigationMenuLink>
+            </div>
+        </NavigationMenu>
+
+        {/* Mobile */}
+        <Sheet>
+            <SheetTrigger className='md:hidden'>☰</SheetTrigger>
+                <SheetContent>
+                    <SheetHeader className='mt-4'>
+                    <SheetTitle>Home</SheetTitle>
+                    <SheetTitle>About</SheetTitle>
+                    <SheetTitle>Skilss</SheetTitle>
+                    <SheetTitle>Projects</SheetTitle>
+                    <SheetTitle>Serticate</SheetTitle>
+                    <SheetTitle>Contact</SheetTitle>
+                    </SheetHeader>
+                </SheetContent>
+        </Sheet>
+    </div>
+  )
+}
