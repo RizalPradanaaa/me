3b42ba08f38d7d8a756e9596f529458db175f7e2
RizalPradanaaa <rizalpradanaaa@gmail.com>
2024-11-24 22:13:06 +0700

    add proeject and link project

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -1,3 +1,4 @@
+import { ExternalLinkIcon, GithubIcon } from 'lucide-react'
 import { Card, CardContent, CardFooter, CardHeader, CardTitle } from './ui/card'
 
 export default function Project() {
@@ -24,11 +25,19 @@ export default function Project() {
             <CardContent className="px-6 italic text-left md:text-justify font-md text-slate-600 dark:text-slate-300">
                 <p>his innovative mobile application can detect diseases through chest X-ray scans and provide accurate diagnoses. In this project, I was responsible for the backend, creating the API to connect the model with the mobile application.</p>
             </CardContent>
-            <CardFooter className="px-6 italic font-md text-left flex gap-4">
+            <CardFooter className="flex flex-col px-6 italic font-md text-left gap-4">
+            <div className="flex gap-4">
                 <img src="/img/js.svg" width={30} alt="JavaScript"/>
                 <img src="/img/nodejs.svg" width={30} alt="Node JS"/>
                 <img src="/img/express.svg" width={30} alt="Express JS"/>
                 <img src="/img/googlecloud.svg" width={30} alt="Google Cloud"/>
+            </div>
+            <div className="flex text-xs gap-4">
+                    <a href="https://github.com/X-Detect" target='_blank' className='items-center underline hover:text-sky-600'>
+                        <GithubIcon size={15} className="inline-block mr-1" />
+                        View source code
+                    </a>
+                </div>
             </CardFooter>
         </Card>
         <Card className="w-full md:w-1/3 shadow-md border-1 rounded-md dark:bg-slate-800 dark:border-slate-500 dark:border-2 dark:rounded-md">
@@ -45,12 +54,20 @@ export default function Project() {
             <CardContent className="px-6 italic text-left md:text-justify font-md text-slate-600 dark:text-slate-300 ">
                 <p>A sales information system website for managing inventory at the Suara Muhammadiyah Mayong Unit. I developed the entire system as a full-stack developer.</p>
             </CardContent>
-            <CardFooter className="px-6 italic font-md text-left flex gap-4">
-                <img src="/img/php.svg" width={30} alt="PHP"/>
-                <img src="/img/js.svg" width={30} alt="JavaScript"/>
-                <img src="/img/codeigniter.svg" width={30} alt="CodeIgniter"/>
-                <img src="/img/bootstrap.svg" width={30} alt="Bootstrap"/>
-                <img src="/img/mysql.svg" width={30} alt="MySQL"/>
+            <CardFooter className="flex flex-col px-6 italic font-md text-left gap-4">
+                <div className="flex gap-4">
+                    <img src="/img/php.svg" width={30} alt="PHP"/>
+                    <img src="/img/js.svg" width={30} alt="JavaScript"/>
+                    <img src="/img/codeigniter.svg" width={30} alt="CodeIgniter"/>
+                    <img src="/img/bootstrap.svg" width={30} alt="Bootstrap"/>
+                    <img src="/img/mysql.svg" width={30} alt="MySQL"/>
+                </div>
+                <div className="flex text-xs gap-4">
+                    <a href="https://github.com/RizalPradanaaa/sm-corner" target='_blank' className='items-center underline hover:text-sky-600'>
+                        <GithubIcon size={15} className="inline-block mr-1" />
+                        View source code
+                    </a>
+                </div>
             </CardFooter>
         </Card>
         <Card className="w-full md:w-1/3 shadow-md border-1 rounded-md dark:bg-slate-800 dark:border-slate-500 dark:border-2 dark:rounded-md">
@@ -67,14 +84,54 @@ export default function Project() {
             <CardContent className="px-6 italic text-left md:text-justify font-md text-slate-600 dark:text-slate-300">
                 <p>A website for early detection of stroke based on relevant labels, providing effective solutions for prevention and early diagnosis.</p>
             </CardContent>
-            <CardFooter className="px-6 italic font-md text-left flex gap-4">
+            <CardFooter className="flex flex-col px-6 italic font-md text-left gap-4">
+                <div className="flex gap-4">
                 <img src="/img/python.svg" width={30} alt="Python"/>
+                </div>
+                <div className="flex text-xs gap-4">
+                    <a href="https://github.com/RizalPradanaaa/StrokePrediction" target='_blank' className='items-center underline hover:text-sky-600'>
+                        <GithubIcon size={15} className="inline-block mr-1" />
+                        View source code
+                    </a>
+                </div>
             </CardFooter>
         </Card>
         <Card className="w-full md:w-1/3 shadow-md border-1 rounded-md dark:bg-slate-800 dark:border-slate-500 dark:border-2 dark:rounded-md">
             <CardHeader className="p-0">
                 <CardTitle>
                     <img src="/img/projects/project4.png" alt="Early detection of diabetes" />
+                </CardTitle>
+                <CardTitle>
+                <h4 className="px-6 py-4 scroll-m-20 text-lg font-semibold tracking-tight text-left">
+                    My Portofolio V1
+                </h4>
+                </CardTitle>
+            </CardHeader>
+            <CardContent className="px-6 italic text-left md:text-justify font-md text-slate-600 dark:text-slate-300">
+            <p>"This is the first portfolio website I developed, showcasing the projects I've worked on and my skills in web development."</p>
+            </CardContent>
+            <CardFooter className="flex flex-col px-6 italic font-md text-left gap-4">
+                <div className="flex gap-4">
+                <img src="/img/html.svg" width={30} alt="HTML"/>
+                <img src="/img/css.svg" width={30} alt="CSS"/>
+                <img src="/img/tailwind.svg" width={30} alt="Tailwind"/>
+                </div>
+                <div className="flex text-xs gap-4">
+                    <a href="https://rizalpradanaaa.github.io/my-portofolio" target='_blank' className='items-center underline hover:text-sky-600'>
+                        <ExternalLinkIcon size={15} className="inline-block mr-1" />
+                        View project
+                    </a>
+                    <a href="https://github.com/rizalpradanaaa/my-portofolio" target='_blank' className='items-center underline hover:text-sky-600'>
+                        <GithubIcon size={15} className="inline-block mr-1" />
+                        View source code
+                    </a>
+                </div>
+            </CardFooter>
+        </Card>
+        <Card className="w-full md:w-1/3 shadow-md border-1 rounded-md dark:bg-slate-800 dark:border-slate-500 dark:border-2 dark:rounded-md">
+            <CardHeader className="p-0">
+                <CardTitle>
+                    <img src="/img/projects/project5.png" alt="Early detection of diabetes" />
                 </CardTitle>
                 <CardTitle>
                 <h4 className="px-6 py-4 scroll-m-20 text-lg font-semibold tracking-tight text-left">
@@ -85,10 +142,18 @@ export default function Project() {
             <CardContent className="px-6 italic text-left md:text-justify font-md text-slate-600 dark:text-slate-300">
                 <p>"Website for early detection of diabetes, a final project for my studies focused on data processing and improving the accuracy of the random forest model using genetic algorithms.</p>
             </CardContent>
-            <CardFooter className="px-6 italic font-md text-left flex gap-4">
+            <CardFooter className="flex flex-col px-6 italic font-md text-left gap-4">
+                <div className="flex gap-4">
                 <img src="/img/python.svg" width={30} alt="Python"/>
                 <img src="/img/mysql.svg" width={30} alt="MySQL"/>
                 <img src="/img/bootstrap.svg" width={30} alt="Bootstrap"/>
+                </div>
+                <div className="flex text-xs gap-4">
+                    <a href="https://github.com/RizalPradanaaa/diabetes-prediction" target='_blank' className='items-center underline hover:text-sky-600'>
+                        <GithubIcon size={15} className="inline-block mr-1" />
+                        View source code
+                    </a>
+                </div>
             </CardFooter>
         </Card>
     </div>
