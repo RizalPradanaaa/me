4fb9f4182e90fb6b22e6003468aba3b6631f1843
RizalPradanaaa <rizalpradanaaa@gmail.com>
2024-11-26 11:20:43 +0700

    add link linkedin

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -54,7 +54,9 @@ export default function Contact() {
                     <a href="https://www.instagram.com/rizalpradana.exe/" target="_blank">
                         <svg className="h-8 w-8 text-dark hover:text-sky-500"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round">  <rect x="2" y="2" width="20" height="20" rx="5" ry="5" />  <path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z" />  <line x1="17.5" y1="6.5" x2="17.51" y2="6.5" /></svg>
                     </a>
-                    <svg className="h-8 w-8 text-dark hover:text-sky-500"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round">  <path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z" />  <rect x="2" y="9" width="4" height="12" />  <circle cx="4" cy="4" r="2" /></svg>
+                    <a href="https://www.linkedin.com/in/rizal-nawang-pradana/" target="_blank">
+                        <svg className="h-8 w-8 text-dark hover:text-sky-500"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round">  <path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z" />  <rect x="2" y="9" width="4" height="12" />  <circle cx="4" cy="4" r="2" /></svg>
+                    </a>
                 </div>
             </div>
         </div>
